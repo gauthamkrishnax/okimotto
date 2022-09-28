fa51bf9d728830cad4e05e0cc3fbb26fd8419c4c
gauthamkrishnax <gauthamkrishnax@gmail.com>
2022-09-28 13:36:21 +0530

    init

---
+++ astro.config.mjs
@@ -0,0 +1,9 @@
+import { defineConfig } from 'astro/config';
+import svelte from "@astrojs/svelte";
+
+import image from "@astrojs/image";
+
+// https://astro.build/config
+export default defineConfig({
+  integrations: [svelte(), image()]
+});
---
+++ postcss.config.cjs
@@ -0,0 +1,3 @@
+/* eslint-disable no-undef */
+// eslint-disable-next-line no-use-before-define
+module.exports = { plugins: [require('autoprefixer'), require('cssnano')] }
